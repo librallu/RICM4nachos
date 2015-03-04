fda84bcdcb0fd74e1d9aa4e0a266316819de38f3
Olivier SOLDANO <olivier.soldano@e.ujf-grenoble.fr>
2015-03-04 09:10:56 +0100

    pre merge commit

---
+++ code/test/putstring.c
@@ -0,0 +1,11 @@
+#include "syscall.h"
+
+void print(char s[],int n){
+  PutString(s,n);
+  PutChar('\n');
+}
+
+int main(){
+  print("haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!haha!",630);
+  Halt();
+}
