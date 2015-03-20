32f048e3797df954ec2a45694ac22ef4ac9a9099
Malek MAMMAR <malekmammar@gmail.com>
2015-03-20 09:23:25 +0100

    [V4] frameprovider update

--- code/userprog/frameprovider.cc
+++ code/userprog/frameprovider.cc
@@ -10,28 +10,54 @@
 FrameProvider::FrameProvider() {
 	bitMap = new BitMap(numPages);
 	pageTable = new TranslationEntry[numPages];
+	bitMap->Mark(0);
 }
 
 FrameProvider::~FrameProvider() {
 	delete bitMap;
 }
 
+/**
+ * The function returns a frame address
+ * Returns : Positive value if everything went well
+ * 			 -1 if there is no more frame available
+ */
 int FrameProvider::GetEmptyFrame() {
 	int frameIndex = bitMap->Find();
 	
-	  pageTable[frameIndex].virtualPage = i; // step 4 action I.4
-	  pageTable[frameIndex].physicalPage = i+1;
+	pageTable[frameIndex].virtualPage = i; // step 4 action I.4
+	pageTable[frameIndex].physicalPage = i+1;
+	
+	pageTable[frameIndex].valid = TRUE;
+	pageTable[frameIndex].use = TRUE;
+	pageTable[frameIndex].dirty = FALSE;
+	// if the code segment was entirely on a separate page, we could set its pages to be read-only
+	pageTable[frameIndex].readOnly = FALSE;	
 
-	  pageTable[frameIndex].valid = TRUE;
-	  pageTable[frameIndex].use = FALSE;
-	  pageTable[frameIndex].dirty = FALSE;
-	  pageTable[frameIndex].readOnly = FALSE;	// if the code segment was entirely on 
-	  // a separate page, we could set its 
-	  // pages to be read-only
+	// zero out the entire address space, to zero the unitialized data segment 
+	// and the stack segment
+    bzero (machine->mainMemory+pageTable[frameIndex].physicalPage*numPages, PageSize);
 
-// zero out the entire address space, to zero the unitialized data segment 
-// and the stack segment
-    bzero (machine->mainMemory+pageTable[frameIndex].physicalPage*numPages, size);
+	return machine->mainMemory+pageTable[frameIndex].physicalPage*numPages;
+}
 
-	return ;
+/**
+ * Release a frame
+ * Returns : 0 if everything went well
+ * 			 -1 if the frameAddress is incorrect
+ */
+int ReleaseFrame(int frameAddress) {
+	int frameIndex = frameAddress / PageSize;
+	if (frameIndex<=numPages) {
+		pageTable[frameIndex].valid = FALSE;
+		pageTable[frameIndex].use = FALSE;
+		pageTable[frameIndex].dirty = FALSE;
+		bitMap->Clear(frameIndex);
+		return 0;
+	}
+	return -1;
+}
+
+int NumAvailFrame() {
+	return bitMap->numClear();
 }
